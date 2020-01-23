6f9b6552d6ffc4c6c07be8021c6859851f41f828
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-01-23 14:42:55 -0400

    Appending the class "Employee"

---
+++ FinalProjectAplicada2/Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectAplicada2.Data
+{
+    public class Employee
+    {
+    }
+}
