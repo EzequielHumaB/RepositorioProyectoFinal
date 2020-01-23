e91c59f7c4e94407d8b1cfb1a07b3cf954e114c7
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-01-23 15:06:33 -0400

    Installacion of Entity framework core to the project

---
+++ FinalProjectAplicada2/Data/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProjectAplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+
+    }
+}
--- FinalProjectAplicada2/Data/Employee.cs
+++ FinalProjectAplicada2/Data/Employee.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinalProjectAplicada2.Data
 {
     public class Employee
     {
+        [Key]
+        public int EmpleadoId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Cedula { get; set; }
+        public string Nacionalidad { get; set; }
+        public String EstadoCivil { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public decimal Salario { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public Employee()
+        {
+            EmpleadoId = 0;
+            Nombres = string.Empty;
+            Apellidos = string.Empty;
+            Cedula = string.Empty;
+            FechaNacimiento = DateTime.Now;
+            Nacionalidad = string.Empty;
+            EstadoCivil = string.Empty;
+            Salario = 0;
+            FechaIngreso = DateTime.Now;
+            Celular = string.Empty;
+            Telefono = string.Empty;
+        }
     }
 }
